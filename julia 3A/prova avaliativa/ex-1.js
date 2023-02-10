d32a9b1092c5dde2f00238d4b985eb72f81b321d
Julia Wenzel de Ramos <99267074+ramosjw@users.noreply.github.com>
2023-02-10 12:47:52 -0300

    Add files via upload

---
+++ julia 3A/prova avaliativa/ex-1.js
@@ -0,0 +1,7 @@
+var a, b, c, d
+var a = 10
+var b = 20
+var c = 10 * 20
+var d = 10 * 20 + 10 + 20
+media = d/4
+console.log ('Questão 1' + "A: " + a, "B: " + b, "C: " + c, "D: " + d, "Média: " + media );
---
+++ julia 3A/prova avaliativa/ex-2.js
@@ -0,0 +1,7 @@
+var salario, nvsalario, valoreajust, reajust
+var salario = 3000
+var reajust = 0.12
+var valoreajust = salario * reajust
+var nvsalario = salario + valoreajust
+
+console.log('Questão 2 ' + "Novo salário: "+ nvsalario, " Valor Reajustado: " + valoreajust,  "Índice Reajustado: " + reajust)
